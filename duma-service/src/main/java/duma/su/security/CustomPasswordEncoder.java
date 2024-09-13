34cf09168b346eb018e59dd6d28794d58fbff5ad
s_antonov02 <s_antonov02@rambler.ru>
2024-09-13 18:15:31 +0500

    Udated password encryption

--- duma-service/src/main/java/duma/su/security/CustomPasswordEncoder.java
+++ duma-service/src/main/java/duma/su/security/CustomPasswordEncoder.java
@@ -10,18 +10,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Component
 public class CustomPasswordEncoder implements PasswordEncoder {
 
+    private BCryptPasswordEncoder encoder;
+    public CustomPasswordEncoder(){
+        encoder = new BCryptPasswordEncoder();
+    }
+
     @Override
     public String encode(CharSequence rawPassword) {
         //шифруем пороль
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String hashedPassword = passwordEncoder.encode(rawPassword);
+        String hashedPassword = encoder.encode(rawPassword);
 
         return hashedPassword;
     }
 
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder.matches(rawPassword, encodedPassword);//rawPassword.equals(encodedPassword);
     }
 }
