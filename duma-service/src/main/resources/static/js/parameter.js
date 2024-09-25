dd91eb4e21a3a97b863f9b0a6d0264109d9af802
s_antonov02 <s_antonov02@rambler.ru>
2024-09-25 13:33:55 +0500

    Added edit referens

--- duma-service/src/main/resources/static/js/parameter.js
+++ duma-service/src/main/resources/static/js/parameter.js
@@ -1,4 +1,34 @@
 
+const { createApp, ref } = Vue
+
+createApp({
+    setup() {
+        let message = ref('Hello vue!')
+
+        function increment() {
+            // .value is needed in JavaScript
+            //message = 'abc'
+            showAlert()
+        }
+
+        return {
+            message,
+            increment
+        }
+    }
+}).mount('#app')
+
+
+/*
+let app = new Vue({
+    el: '#contents',
+    methods: {
+        eventUpdate() {
+            showAlert();
+        }
+    }
+});*/
+
 //const body = document.getElementById("element");
 
 function showAlert() {
