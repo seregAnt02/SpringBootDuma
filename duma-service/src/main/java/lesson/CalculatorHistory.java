5f76adb3e61d233f7131b1fc5d62431f80bcfd31
s_antonov02 <s_antonov02@rambler.ru>
2024-10-13 14:23:04 +0500

    Added discription project(read.md)

--- duma-service/src/main/java/lesson/CalculatorHistory.java
+++
@@ -1,8 +0,0 @@
-package lesson;
-
-public class CalculatorHistory {
-
-    public void logSumOperation(int a, int b, int result){
-        System.out.println(String.format("%s = %s = %s", a, b, result));
-    }
-}
