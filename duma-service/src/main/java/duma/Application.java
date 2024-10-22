497f0335313444bbd8244f924b0a04cc399925a9
s_antonov02 <s_antonov02@rambler.ru>
2024-10-22 16:31:19 +0500

    Update project

--- duma-service/src/main/java/duma/Application.java
+++ duma-service/src/main/java/duma/Application.java
@@ -10,6 +10,5 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
 
-        //System.out.println("Hello world!");
     }
 }
