6f6f64c6af57c82a87e325efae78e4a3939fa620
Gb <s_antonov02@rambler.ru>
2024-10-22 16:22:27 +0400

    Git test unix

--- duma-service/src/main/java/duma/Application.java
+++ duma-service/src/main/java/duma/Application.java
@@ -9,6 +9,6 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
-
+        // git test unix
     }
 }
